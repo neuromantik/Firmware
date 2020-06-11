1a812bf964345553cbeee05af072be6d1c320691
Daniel Agar <daniel@agar.ca>
2020-06-11 15:52:50 -0400

    tone alarm cleanup

--- src/systemcmds/tests/test_tone.cpp
+++ src/systemcmds/tests/test_tone.cpp
@@ -39,9 +39,9 @@
 #include <stdlib.h>
 
 #include <drivers/drv_hrt.h>
-#include <drivers/drv_tone_alarm.h>
 #include <lib/tunes/tunes.h>
 #include <px4_platform_common/posix.h>
+#include <uORB/Publication.hpp>
 #include <uORB/topics/tune_control.h>
 
 #include "tests_main.h"
@@ -52,8 +52,7 @@ int test_tone(int argc, char *argv[])
 	tune_control_s tune_control = {};
 	tune_control.tune_id = static_cast<int>(TuneID::NOTIFY_NEGATIVE);
 
-	orb_advert_t tune_control_pub = orb_advertise_queue(ORB_ID(tune_control), &tune_control,
-					tune_control_s::ORB_QUEUE_LENGTH);
+	uORB::PublicationQueued<tune_control_s> tune_control_pub{ORB_ID(tune_control)};
 
 	if (argc == 1) {
 		PX4_INFO("Volume silenced for testing predefined tunes 0-20.");
@@ -61,9 +60,9 @@ int test_tone(int argc, char *argv[])
 
 		for (size_t i = 0; i <= 20; i++) {
 			tune_control.tune_id = i;
-			result = orb_publish(ORB_ID(tune_control), tune_control_pub, &tune_control);
+			tune_control.timestamp = hrt_absolute_time();
 
-			if (result != PX4_OK) {
+			if (!tune_control_pub.publish(tune_control)) {
 				PX4_INFO("Error publishing TuneID: %d", tune_control.tune_id);
 				return result;
 			}
@@ -88,8 +87,8 @@ int test_tone(int argc, char *argv[])
 	}
 
 	tune_control.volume = tune_control_s::VOLUME_LEVEL_DEFAULT;
-	result = orb_publish(ORB_ID(tune_control), tune_control_pub, &tune_control);
+	tune_control.timestamp = hrt_absolute_time();
+	tune_control_pub.publish(tune_control);
 
-	orb_unadvertise(tune_control_pub);
 	return result;
 }
